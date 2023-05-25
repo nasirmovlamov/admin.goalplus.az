21caa755e05b347c6f4b8b3479b68b6467295d82
Nasir Movlamov <movlamovnasir2000@gmail.com>
2023-05-26 01:14:15 +0400

    feat: gender

--- src/components/leagues/CreateLeagueModal.tsx
+++ src/components/leagues/CreateLeagueModal.tsx
@@ -317,6 +317,7 @@ export const CreateLeagueModal = ({ sportId, modal, setModal }: any) => {
             >
               <option value="Male">Male</option>
               <option value="Female">Female</option>
+              <option value="Unisex">Unisex</option>
             </select>
             <span className=" text-red-500">
               {errors?.leagueDetails?.gender && "gender is required"}
